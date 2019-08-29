9f723c68b8ff107c8e669d2e5e7c0ce6a4765e96
tipakA <emka456@onet.pl>
2019-08-29 06:49:53 +0200

    Add eval command + helper function

--- tipakBot.js
+++ tipakBot.js
@@ -4,6 +4,17 @@ require('dotenv').config();
 
 client.config = require('./config.json');
 
+async function clear(input) {
+  let output = input;
+  if (input && input.constructor.name === 'Promise') output = await output;
+  if (typeof evaled !== 'string') output = require('util').inspect(input, { depth: 1 }); /* eslint-disable-line global-require */
+
+  return output
+    .replace(/`/g, `\`${String.fromCharCode(8203)}`)
+    .replace(/@/g, `@${String.fromCharCode(8203)}`)
+    .replace(client.token, '👌');
+}
+
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}.`));
 
 client.on('message', async message => {
@@ -17,6 +28,16 @@ client.on('message', async message => {
   if (command === 'ping') {
     if (!message.channel.permissionsFor(client.user).has('SEND_MESSAGES')) return;
     message.reply('Pong!');
+  } else if (command === 'eval') {
+    if (message.author.id !== client.config.owner) return;
+    const code = args.join(' ');
+    try {
+      const evaled = eval(code);
+      const clean = await clear(evaled);
+      message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
+    } catch (err) {
+      message.channel.send(`\`\`\`xl\n${await clear(err)}\n\`\`\``);
+    }
   }
 });
 
