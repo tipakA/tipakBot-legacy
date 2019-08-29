c5505ccbf58dca32828cab5387130df9c3b72408
tipakA <emka456@onet.pl>
2019-08-29 05:52:35 +0200

    Added config

--- tipakBot.js
+++ tipakBot.js
@@ -2,6 +2,8 @@ const { Client } = require('discord.js');
 const client = new Client({ disableEveryone: true });
 require('dotenv').config();
 
+client.config = require('./config.json');
+
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}.`));
 
 client.login(process.env.TESTTOKEN);
